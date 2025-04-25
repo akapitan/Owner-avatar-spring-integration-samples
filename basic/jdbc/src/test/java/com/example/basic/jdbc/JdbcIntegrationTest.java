35f585757a929fa6ce212e89c0cb9e697e4f14e9
Aleksandar Kapitan <aleksandar.kapitan@infinum.hr>
2025-04-25 18:20:19 +0200

    Fix jdbc integration tests

--- basic/jdbc/src/test/java/com/example/basic/jdbc/JdbcIntegrationTest.java
+++ basic/jdbc/src/test/java/com/example/basic/jdbc/JdbcIntegrationTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 public class JdbcIntegrationTest {
 
     @Autowired
